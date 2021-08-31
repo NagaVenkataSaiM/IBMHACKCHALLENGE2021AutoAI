f71120b72628d8261161303872dae7421263cb11
Naga Venkata sai m <87435205+NagaVenkataSaiM@users.noreply.github.com>
2021-08-31 13:50:42 +0530

    PriceModel

--- DecisionTreeRegressoreg.py
+++ DecisionTreeRegressoreg.py
@@ -6,13 +6,15 @@ datafile_data=pd.read_csv(datafile_path)
 print(datafile_data.columns)
 datafile_data=datafile_data.dropna(axis=0)
 y=datafile_data.Supportprice
+datafile_features=['Crop','CostofCultivation','CostofCultivation2','CostofProduction','Yield']
+X=datafile_data[datafile_features]
+X.describe()
+print("The top values for prediction validation are:")
+print(X.head())
 datafile_features=['CostofCultivation','CostofCultivation2','CostofProduction','Yield']
 X=datafile_data[datafile_features]
 X.describe()
-print(X.head())
 datafile_model=DecisionTreeRegressor(random_state=0)
 datafile_model.fit(X,y)
-print("Making prediction of 5 states:")
-print(X.head())
 print("The predictions are")
 print(datafile_model.predict(X.head()))
